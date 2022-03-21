b979eb050a9017db29e01be1376d6105c61ab3c4
rodney22@live.nl <rodney22@live.nl>
2022-03-21 23:44:59 +0100

    add env vars

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    return (
+      'Hello World!' + this.configService.get('DATABASE_CONNECTION_STRING')
+    );
   }
 }
